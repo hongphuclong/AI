1b4f81490de7487e3d80cbf65f35d2f21cfdf0df
hongphuclong <120164252+hongphuclong@users.noreply.github.com>
2023-10-02 21:18:41 +0700

    Add files via upload

---
+++ Lab1_Task2/NoOpAction.java
@@ -0,0 +1,13 @@
+package Lab1_Task2; 
+
+public class NoOpAction extends Action {
+	public static final NoOpAction NO_OP = new NoOpAction();
+
+	public boolean isNoOp() {
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "No Action";
+	}
+}
